439b333baa31386b2a33049b365b57b276c6675b
Alex Mordue <alex.mordue@hotmail.com.au>
2019-12-08 22:04:01 +1100

    Updates

---
+++ precache-manifest.e0857e1418424eb0be02f1e7ddbc4a0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f5a31fb3310b2d652ff8af83e422a9",
+    "url": "/material-poc/index.html"
+  },
+  {
+    "revision": "f0a1586dda1e47d396e5",
+    "url": "/material-poc/static/css/main.a026e173.chunk.css"
+  },
+  {
+    "revision": "78ba61aaa856640bac39",
+    "url": "/material-poc/static/js/2.2acc9a78.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/material-poc/static/js/2.2acc9a78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0a1586dda1e47d396e5",
+    "url": "/material-poc/static/js/main.3f0cc7ed.chunk.js"
+  },
+  {
+    "revision": "74e4157fec51d341a029",
+    "url": "/material-poc/static/js/runtime-main.abf67ff9.js"
+  },
+  {
+    "revision": "8a8d94afa884be87fba8ed807a911132",
+    "url": "/material-poc/static/media/Gordita Medium.8a8d94af.woff"
+  },
+  {
+    "revision": "1af86f05f46db70e0dc24e719472409a",
+    "url": "/material-poc/static/media/Gordita Regular.1af86f05.woff"
+  }
+]);
